365953417f42601d5593d4f288f70cd14ebbe41d
Josef Handl <handljosef@ics.muni.cz>
2020-12-03 11:57:15 +0100

    move the Dockerfile and other files to a separate master-node dir

--- master-node/docker-entrypoint.sh
+++ master-node/docker-entrypoint.sh
@@ -16,8 +16,7 @@ echo $WEBPORT
 echo $DISPLAY
 
 # install all installed plugins
-for pl in $(cat ${S_USER_HOME}/plugin-list-pl.txt); do ${S_USER_HOME}/scipion3/scipion3 installp -p $pl -j $CORE_COUNT --noBin; done
-#for bin in $(cat ${S_USER_HOME}/plugin-list-bin.txt); do ${S_USER_HOME}/scipion3/scipion3 installb $bin -j $CORE_COUNT; done
+for pl in $(cat ${S_USER_HOME}/plugin-list.txt); do ${S_USER_HOME}/scipion3/scipion3 installp -p $pl -j $CORE_COUNT --noBin; done
 
 mkdir $S_USER_HOME/.vnc
 echo $MYVNCPASSWORD
